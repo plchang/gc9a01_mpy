fd9cab7fb96cd530f76a9fef3a2b8c4b786d0ad4
plchang <149220351+plchang@users.noreply.github.com>
2024-01-17 23:04:06 +0800

    Update setup .py

--- setup (2).py
+++ setup (2).py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='st7789py_mpy',
+    name='gc9a01_mpy',
     version='0.1',
     packages=find_packages(),
-    package_dir={'st7789py_mpy'},
-    entry_points={ 'console_scripts': ['st7789py_mpy =__main__:main' ] }
+    package_dir={'gc9a01_mpy'},
+    entry_points={ 'console_scripts': ['gc9a01_mpy =__main__:main' ] }
 )
